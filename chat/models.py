ed71744280158dae2264a01cc5bba8ac4e5b7694
Ashish Pradhan <ashishprdhan892@gmail.com>
2021-07-05 19:15:26 +0530

    db update

--- chat/models.py
+++ chat/models.py
@@ -11,11 +11,11 @@ class Room(models.Model):
         return "Room Id: {}".format(self.name)
 
 class Message(models.Model):
-    username = models.CharField()
+    username = models.CharField(max_length=50)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
-    message = models.CharField()
-    date = models.CharField(blank=True, default=datetime.now().strftime('%d/%B/%Y'))
-    createdAt = models.CharField(blank=True, default=datetime.now().strftime('%I:%M %p'))
+    message = models.TextField(max_length=500)
+    date = models.CharField(max_length=20, blank=True, default=datetime.now().strftime('%d/%B/%Y'))
+    createdAt = models.CharField(max_length=10, blank=True, default=datetime.now().strftime('%I:%M %p'))
 
     def __str__(self) -> str:
         return "user: {} | {} | at: {}".format(self.username, self.room, self.createdAt)
