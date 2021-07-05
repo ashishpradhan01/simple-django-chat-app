1fb97c95705a47f4e5b354618f00713d9fecfa83
Ashish Pradhan <ashishprdhan892@gmail.com>
2021-07-05 19:55:37 +0530

    db updated date

--- chat/models.py
+++ chat/models.py
@@ -14,7 +14,7 @@ class Message(models.Model):
     username = models.CharField(max_length=10)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
     message = models.TextField(max_length=255)
-    date = models.DateField(blank=True, default=datetime.now().strftime('%d/%B/%Y'))
+    date = models.DateTimeField(blank=True, default=datetime.now())
     createdAt = models.CharField(max_length=10, blank=True, default=datetime.now().strftime('%I:%M %p'))
 
     def __str__(self) -> str:
