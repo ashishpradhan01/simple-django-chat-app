763f9395c3e2bba7ee707ab660af2bfa1d3f3a00
Ashish Pradhan <ashishprdhan892@gmail.com>
2021-07-05 21:22:26 +0530

    model update

--- chat/models.py
+++ chat/models.py
@@ -4,18 +4,18 @@ from django.utils import timezone
 # Create your models here.
 
 class Room(models.Model):
-    name = models.CharField(max_length=20)
-    datetime = models.DateTimeField(blank=True, default=timezone.now())
+    name = models.CharField(max_length=20,null=False)
+    datetime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return "Room Id: {}".format(self.name)
 
 class Message(models.Model):
-    username = models.CharField(max_length=20)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
-    message = models.TextField()
-    date = models.DateTimeField(blank=True, default=timezone.now())
-    createdAt = models.TimeField(blank=True, default=datetime.now().strftime('%I:%M %p'))
+    username = models.CharField(max_length=20, null=False)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages',null=False)
+    message = models.TextField(null=False)
+    date = models.DateTimeField(auto_now_add=True, null=False)
+    createdAt = models.CharField(default=datetime.now().strftime('%I:%M %p'),null=False, max_length=255)
 
     def __str__(self) -> str:
         return "user: {} | {} | at: {}".format(self.username, self.room, self.createdAt)
