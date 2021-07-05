76d27cde70db389fce1ab0a36aa1db9fca979615
Ashish Pradhan <ashishprdhan892@gmail.com>
2021-07-05 20:36:19 +0530

    date updated

--- chat/models.py
+++ chat/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from datetime import datetime
-
+from django.utils import timezone
 # Create your models here.
 
 class Room(models.Model):
     name = models.CharField(max_length=10)
-    datetime = models.DateTimeField(blank=True, default=datetime.now())
+    datetime = models.DateTimeField(blank=True, default=timezone.now())
 
     # def __str__(self) -> str:
     #     return "Room Id: {}".format(self.name)
@@ -14,7 +14,7 @@ class Message(models.Model):
     username = models.CharField(max_length=10)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
     message = models.TextField(max_length=255)
-    date = models.DateTimeField(blank=True, default=datetime.now())
+    date = models.DateTimeField(blank=True, default=timezone.now())
     createdAt = models.CharField(max_length=10, blank=True, default=str(datetime.now().strftime('%I:%M %p')))
 
     # def __str__(self) -> str:
