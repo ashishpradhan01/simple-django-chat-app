53150721bad60728be742a07182543707be69da4
Ashish Pradhan <ashishprdhan892@gmail.com>
2021-07-05 18:27:20 +0530

    setting change

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'djangochat.urls'
