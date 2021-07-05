8b43ab67dede495d674938ace88204623e2b0a2e
Ashish Pradhan <ashishprdhan892@gmail.com>
2021-07-05 19:05:37 +0530

    remove limits models

--- chat/models.py
+++ chat/models.py
@@ -4,18 +4,18 @@ from datetime import datetime
 # Create your models here.
 
 class Room(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField()
     datetime = models.DateTimeField(blank=True, default=datetime.now())
 
     def __str__(self) -> str:
         return "Room Id: {}".format(self.name)
 
 class Message(models.Model):
-    username = models.CharField(max_length=50)
+    username = models.CharField()
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
-    message = models.CharField(max_length=500)
-    date = models.CharField(max_length=20, blank=True, default=datetime.now().strftime('%d/%B/%Y'))
-    createdAt = models.CharField(max_length=10, blank=True, default=datetime.now().strftime('%I:%M %p'))
+    message = models.CharField()
+    date = models.CharField(blank=True, default=datetime.now().strftime('%d/%B/%Y'))
+    createdAt = models.CharField(blank=True, default=datetime.now().strftime('%I:%M %p'))
 
     def __str__(self) -> str:
         return "user: {} | {} | at: {}".format(self.username, self.room, self.createdAt)
